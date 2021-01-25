e801b07080e5e7242d5d1d49e4c697d3d867c80a
X9Squared <justin.wang91202@gmail.com>
2021-01-25 12:53:15 -0500

    ScreenPos to get mouse location on right click

---
+++ Assets/Scenes/ScreenPos.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+
+public class ContextMenu : MonoBehaviour, IPointerClickHandler
+{
+    public Transform target;
+    public static Vector3 screenPos;
+    public static bool showMenu;
+    Camera cam;
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.button == PointerEventData.InputButton.Right) {
+            Debug.Log("Right Mouse Button Clicked");
+            screenPos = cam.WorldToScreenPoint(target.position);
+            Debug.Log("target is " + screenPos.x + " pixels from the left");
+            showMenu = true;
+        }
+    }
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        
+    }
+    
+}
