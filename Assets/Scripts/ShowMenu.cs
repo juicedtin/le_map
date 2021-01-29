ecacbe8eca7c5034dc315aca50e68a7d2799656f
X9Squared <justin.wang91202@gmail.com>
2021-01-28 21:16:18 -0500

    Menu instantiation on right click

--- Assets/Scripts/ShowMenu.cs
+++ Assets/Scripts/ShowMenu.cs
@@ -8,17 +8,18 @@ public class ShowMenu : MonoBehaviour, IPointerClickHandler
 {
     public Vector3 pointerPos;
     public Camera cam;
+    public Transform menuParent;
     public GameObject menuPrefab;
-    public bool menuShown = false;
     public void OnPointerClick(PointerEventData eventData)
     {
         if (eventData.button == PointerEventData.InputButton.Right)
         {
             Debug.Log("Right Mouse Button Clicked");
-            pointerPos = Input.mousePosition;
-            menuShown = true;
+            Vector2 mouseWorldPosition = Input.mousePosition;
+            pointerPos = cam.ScreenToWorldPoint(new Vector3(mouseWorldPosition.x, mouseWorldPosition.y, cam.nearClipPlane));
             Debug.Log("target is at (" + pointerPos.x + ", " + pointerPos.y + ")");
-            Instantiate(menuPrefab, pointerPos, Quaternion.identity);
+            Instantiate(menuPrefab, pointerPos, Quaternion.identity, menuParent);
+            Debug.Log("object instantiated");
         }
     }
 
