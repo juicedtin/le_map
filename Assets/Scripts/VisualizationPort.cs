637e613da0a081d78da39627a97e46bf791ac685
X9Squared <justin.wang91202@gmail.com>
2021-08-30 16:11:57 -0700

    Color interp and value assignment for VisPort

--- Assets/Scripts/VisualizationPort.cs
+++ Assets/Scripts/VisualizationPort.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class VisualizationPort : MonoBehaviour
 {
     string targetTag;
     GameObject targetObject;
+    float eMin;
+    float eMax;
+    float fMax;
+    float fMin;
     /*Case-based colors, current implementation is done with color lerp
     Color none = new Color(0.3114324, 0.7735849, 0.0105415, 1);
     Color mild = new Color(0.8962264, 0.7194716. 0, 1);
@@ -23,13 +28,32 @@ public class VisualizationPort : MonoBehaviour
         
     }
 
-    void RedCapPort(int eVal, int fVal, bool drs, GameObject targetObject)
+
+    void RedCapPort(float eVal, float fVal, bool drs, GameObject targetObject)
     {
-        GameObject eSlider = targetObject.transform.Find("EdemaSlider");
-        GameObject fSlider = targetObject.transform.Find("FatSlider");
-        GameObject drsBox = targetObject.transform.Find("DRS");
-        eSlider.value = eVal;
-        fSlider.value = fVal;
-        drsBox.isOn = drs;
+        //Find and assign data to each GameObject for visualization
+        GameObject eSlider = targetObject.transform.Find("EdemaSlider").gameObject;
+        GameObject fSlider = targetObject.transform.Find("FatSlider").gameObject;
+        GameObject drsBox =  targetObject.transform.Find("DRS").gameObject;
+        eSlider.GetComponent<Slider>().value = eVal;
+        fSlider.GetComponent<Slider>().value = fVal;
+        drsBox.GetComponent<Toggle>().isOn = drs;
+
+        //Set slider color for easy viewing
+        ColorBlock eLerp = eSlider.GetComponent<Slider>().colors;
+        eLerp.normalColor = Color.Lerp(Color.green, Color.red, (eVal - eMin) / (eMax - eMin));
+        eSlider.GetComponent<Slider>().colors = eLerp;
+        ColorBlock fLerp = fSlider.GetComponent<Slider>().colors;
+        fLerp.normalColor = Color.Lerp(Color.green, Color.red, (eVal - eMin) / (eMax - eMin));
+        fSlider.GetComponent<Slider>().colors = fLerp;
+        ColorBlock drsLerp = new ColorBlock();
+        if (drs)
+        {
+            drsLerp.normalColor = Color.red;
+        } else
+        {
+            drsLerp.normalColor = Color.white;
+        }
+        drsBox.GetComponent<Toggle>().colors = drsLerp;
     }
 }
